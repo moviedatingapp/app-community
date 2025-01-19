3e0f0b2aca16e9ecce6ff18a71b1ff9549543d73
sanskarchoudhry <sanskar22101@gmail.com>
2025-01-19 19:10:28 +0530

    fix: css fix selected styling textarea

--- src/app/create-post/_components/TextArea.tsx
+++ src/app/create-post/_components/TextArea.tsx
@@ -1,29 +1,51 @@
 "use client";
 import { editableIcons } from "@/data";
 import Image from "next/image";
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 
 const TextArea: React.FC = () => {
   const editorRef = useRef<HTMLDivElement>(null);
+  const [activeFormats, setActiveFormats] = useState<string[]>([]);
 
+  // Function to apply formatting
   const applyFormat = (command: string, value?: string) => {
     if (editorRef.current && document.queryCommandSupported(command)) {
       document.execCommand(command, false, value);
       editorRef.current.focus();
+      updateActiveFormats(); // Update active formats after applying the style
     } else {
       console.warn(`Command "${command}" is not supported.`);
     }
   };
 
+  // Function to check and update active formats
+  const updateActiveFormats = () => {
+    const newActiveFormats: string[] = [];
+    editableIcons.forEach((icon) => {
+      if (document.queryCommandState(icon.iconApplyFormat)) {
+        newActiveFormats.push(icon.iconApplyFormat);
+      }
+    });
+    setActiveFormats(newActiveFormats);
+  };
+
+  // Check active formats on mount and whenever the content in the editor changes
+  useEffect(() => {
+    updateActiveFormats();
+  }, []);
+
   return (
-    <div className="w-full  border-light-grey-border rounded-24 border">
+    <div className="w-full border-light-grey-border rounded-24 border">
       <div className="flex gap-14 p-3">
         {editableIcons.map((editableIcon, index) => {
+          const isActive = activeFormats.includes(editableIcon.iconApplyFormat);
           return (
             <button
               key={index}
               type="button"
-              className=" p-2 bg-gray-200 hover:bg-gray-300 rounded-full hover:bg-light-grey"
+              className={`p-2 rounded-full hover:bg-light-grey ${
+                isActive ? "bg-light-grey" : "bg-gray-200 hover:bg-gray-300"
+              }`}
               onClick={() => applyFormat(editableIcon.iconApplyFormat)}
             >
               <Image
