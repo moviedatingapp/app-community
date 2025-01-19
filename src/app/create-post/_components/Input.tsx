a220a91a87fc48219c058871e16aed18bd514981
sanskarchoudhry <sanskar22101@gmail.com>
2025-01-19 20:16:05 +0530

    fix: css fix input

--- src/app/create-post/_components/Input.tsx
+++ src/app/create-post/_components/Input.tsx
@@ -1,13 +1,31 @@
+"use client";
 import React from "react";
+import { useFormContext } from "react-hook-form";
 
 export default function Input() {
+  const { register } = useFormContext();
   return (
-    <div className=" w-full px-4 py-4 border border-light-grey-border rounded-24">
+    <div className="w-full p-4 border border-light-grey-border rounded-24 relative peer">
       <input
-        placeholder="Title"
         type="text"
-        className="w-full outline-none text-dark-grey placeholder:text-light-grey-placeholder"
+        id="inputText"
+        className="w-full outline-none text-dark-grey placeholder-transparent peer px-1 focus:ring-0 focus:border-none"
+        placeholder="Title"
+        {...register("postInput")}
       />
+      <label
+        htmlFor="inputText"
+        className="transition-all text-[#817870] duration-200  absolute peer-focus:top-0 peer-focus:-translate-y-[2px] peer-focus:scale-75 peer-focus:text-dark-black left-4"
+      >
+        Title<span className="text-red-bright">*</span>
+      </label>
+
+      <style jsx>{`
+        .peer:focus-within {
+          border-color: #0766ad;
+          border-width: 2px;
+        }
+      `}</style>
     </div>
   );
 }
